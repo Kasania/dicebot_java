5eeb7811214363563013c4431c3c5ef44bb9cb36
Kasania <nasang1997@gmail.com>
2022-05-01 02:25:08 +0900

    Prepare for slash command

--- src/main/java/com/kasania/Main.java
+++ src/main/java/com/kasania/Main.java
@@ -6,10 +6,16 @@
 package com.kasania;
 
 import com.kasania.dicebot.common.DiceBot;
+import com.kasania.dicebot.v1.Commands;
+import com.kasania.dicebot.v2.DiceBot2;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import net.dv8tion.jda.internal.interactions.command.CommandImpl;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
@@ -28,6 +34,19 @@ public class Main {
             JDA jda = JDABuilder.createDefault(TOKEN).setActivity(Activity.listening("!rhelp")).build();
             jda.addEventListener(new DiceBot());
             log.info("Dice bot is initialized");
+
+            new Thread(() -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                for (Guild guild : jda.getGuilds()) {
+                    log.info("{} : {}", guild.getName(),guild.getIdLong());
+                }
+            }).start();
+
         } catch (IOException | LoginException e) {
             log.error("{}",e.getMessage(),e);
         }
